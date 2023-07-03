981c7a463994fe1f1dce402fd865e55d667c9b8a
Raihan Uddin <raihanemon2015@gmail.com>
2023-07-03 11:42:54 +0200

    implement local session and proteced route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // GET /protected
-  @Get('protected')
-  getHello(): string {
-    return this.appService.getHello();
+  // POST /login
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return { msg: 'Logged in!' };
   }
 
-  // POST /login
-  @Post('login')
-  login(): any {
-    return {};
+  // GET /protected
+  @UseGuards(AuthenticatedGuard)
+  @Get('protected')
+  getHello(@Request() req): string {
+    return req.user;
   }
 }
